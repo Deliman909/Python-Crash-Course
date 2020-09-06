c49219873745d1a30efcb228dd0b0b71c87206ad
Deliman909 <63097753+Deliman909@users.noreply.github.com>
2020-09-06 20:11:33 +0300

    Add files via upload

---
+++ Looping 1.1.py
@@ -0,0 +1,5 @@
+magicians =["Allice", "David", "Carolina"]
+for magician in magicians:
+    print(f"{magician}, that was a great trick")
+    print (f" I can't wait to see your next trick {magician}\n")
+print("Thank you, everyone. That was a great magic show! ")
