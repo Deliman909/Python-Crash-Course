2491b0ccb8a0f6720f1102c71af8513b60e77981
Deliman909 <63097753+Deliman909@users.noreply.github.com>
2020-09-07 22:52:54 +0300

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,5 @@
+pizzas = ["peperoni", "diavola", "mexico"]
+for pizza in pizzas:
+    pizzaa = pizza
+    print (f"I like {pizza} pizza.")
+print ("I really like pizza!")
---
+++ 4.2.py
@@ -0,0 +1,4 @@
+animals = ["Cal", "Vaca", "Bou"]
+for animal in animals:
+    print(f"I really like {animal}")
+print ("I really think they are fun")
